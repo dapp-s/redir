e2b9aa244bfcba25ed6fcfd5d996db2336ba8d48
dapp-s <103443358+dapp-s@users.noreply.github.com>
2025-09-29 10:23:46 +1000

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.redirect(301, 'https://www.new-website.com');
+  res.redirect(301, 'https://mr3wes.xyz/');
 });
 
 app.listen(port, () => {
